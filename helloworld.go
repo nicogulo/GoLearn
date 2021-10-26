1cf437c06e012a824f3481e0eebf8bb5d10debba
nicogulo <nicogulobelas@gmail.com>
2021-10-26 11:37:56 +0700

    Initial commit

---
+++ helloworld.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("Hello World")
+	// fmt.Println("satu = ", 1)
+	// fmt.Println("Dua = ", 2)
+	// fmt.Println("Tiga koma satu = ", 3.1)
+	// fmt.Println(len("Nic"))
+	var name string
+	name = "Nic"
+
+	fmt.Println(name)
+
+}
